d4c2722d92454eea1e6d8f476c4bfe076c87edf6
tiffany <ting11222001@gmail.com>
2025-07-10 07:42:33 +0930

    Bind a formGroup to an HTML form, retrieve the data and pass it to other parts of the app using service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,18 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 /**
  * Notes:
+ * this.route.snapshot.params['id']:
  * Pass in the current id of housing location from url to this component.
  * ActivatedRoute is a reference to the current route we're matched against in the application. From this, we can get access to the route info.
+ * 
  * Replace the initial property, housingLocationId = 0; and make a const variable in the constructor so that we can use it as a parameter to pass in the service.
  * The question mark in housingLocation?.id is called 'optional chaining operator'. It safe-guards calling .id from undefined so that there won't be an error.
+ * 
+ * [formGroup] is a property binding which can be used to bind a formGroup to a <form>.
+ * (submit) is an event binding which can be used to bind to a click event.
+ * The ?? operator is called the nullish coalescing operator. If the value on the left hand side is null or undefined, then the value on the right hand side is used.
  */
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article>
       <img class="listing-photo" [src]="housingLocation?.photo" alt="">
@@ -32,7 +39,18 @@ import { HousingLocation } from '../housing-location';
       </section>
       <section class="listing-apply">
         <h2 class="section-heading">Apply Now to live here</h2>
-        <button class="primary" type="button">Apply</button>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
+          
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName">
+
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email">
+          
+          <button class="primary" type="submit">Apply</button>
+        </form>
       </section>
     </article>
   `,
@@ -43,10 +61,23 @@ export class DetailsComponent {
   // housingLocationId = 0;
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
 
   constructor() {
     // this.housingLocationId = Number(this.route.snapshot.params['id']);
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
   }
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HousingLocation } from './housing-location';
+import { last } from 'rxjs';
 
 /**
  * Notes:
@@ -121,5 +122,9 @@ export class HousingService {
 
   getHousingLocationById(id: Number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(firstName, lastName, email);
   }
 }
